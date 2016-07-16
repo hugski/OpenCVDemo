f751c4a92038d28723df5cc495e7cbea37072ca8
zpf <zpf416@hotmail.com>
2016-07-16 16:07:40 +0800

    cmake to make opencv project

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <opencv/cv.h>
+#include <iostream>
+
+
+int main(int argc, char** argv){
+    cv::Mat img = cv::imread(argv[1], -1);
+    if( img.empty())
+        return -1;
+        
+    cv::namedWindow ("Example1",cv::WINDOW_AUTOSIZE);
+    cv::imshow("Example1", img);
+    cv::waitKey(0);
+    cv::destroyWindow("Example1");
+}
+
